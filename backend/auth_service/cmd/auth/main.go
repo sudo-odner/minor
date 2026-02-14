a1ab8eb7f83ef70716b1857a2ee85afcc725f567
stepan41k <russia.zont@gmail.com>
2026-02-15 02:30:41 +0300

    feat: add initialization of logger, server, etc.

--- backend/auth_service/cmd/auth/main.go
+++ backend/auth_service/cmd/auth/main.go
@@ -1,1 +1,75 @@
 package main
+
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/sudo-odner/min/backend/auth-service/internal/app"
+	"github.com/sudo-odner/min/backend/auth-service/internal/app/http"
+	"github.com/sudo-odner/min/backend/auth-service/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting authentication service")
+
+	router := chi.NewRouter()
+
+	router.Use(middleware.RequestID)
+	router.Use(cors.NewCORS)
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+
+	application := app.New(log, cfg, router)
+
+	go func() {
+		application.HTTPServer.Run()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
+
+	signal := <-stop
+
+	log.Info("stopping application", slog.String("signal", signal.String()))
+
+	application.HTTPServer.Stop(context.Background())
+
+	log.Info("application stopped")
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
