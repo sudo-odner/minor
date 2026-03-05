19fa3bdb28302a80301a2f5d85eb2a7072317a10
stepan41k <russia.zont@gmail.com>
2026-03-06 01:34:27 +0300

    feat: add implementation of middleware access token checking

--- backend/auth_service/internal/http-server/middleware/jwt-check/jwt-check.go
+++ backend/auth_service/internal/http-server/middleware/jwt-check/jwt-check.go
@@ -1,27 +1,49 @@
 package jwtcheck
 
 import (
+	"context"
 	"net/http"
+	"strings"
 
-	"github.com/gin-gonic/gin/render"
 	"github.com/go-chi/render"
+	"github.com/sudo-odner/min/backend/auth-service/internal/config"
+	"github.com/sudo-odner/min/backend/auth-service/internal/lib/api/response"
+	"github.com/sudo-odner/min/backend/auth-service/internal/lib/jwt"
 )
 
 
-func JWTCheck(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token, err := cookie.CheckCookie(w, r)
-		if err != nil || token == nil {
-			render.Status(r, http.StatusUnauthorized)
+func AuthMiddleware(cfg config.TokenConfig) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			token := extractToken(r)
+			claims, err := jwt.ValidateAccessToken(cfg, token)
+			if err != nil {
+				render.Status(r, http.StatusUnauthorized)
 
-			render.JSON(w, r, resp.ErrorResponse{
-				Status: http.StatusUnauthorized,
-				Error: "user unauthorized",
-			})
+				render.JSON(w, r, response.ErrorResponse{
+					Status: http.StatusUnauthorized,
+					Error: "user unauthorized",
+				})
 
-			return
-		}
+				return
+			}
 
-		next.ServeHTTP(w, r)
-	})
+			ctx := context.WithValue(r.Context(), "user_id", claims.UserID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	}
+}
+
+func extractToken(r *http.Request) string {
+	authHeader := r.Header.Get("Authorization")
+	if authHeader == "" {
+		return ""
+	}
+
+	parts := strings.Split(authHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return ""
+	}
+
+	return parts[1]
 }
