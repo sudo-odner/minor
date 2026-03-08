e190359608ad079be7722b6768f2de78c4a843e4
stepan41k <russia.zont@gmail.com>
2026-03-08 23:31:46 +0300

    feat: create interface for auth handler

--- backend/auth_service/internal/http-server/handler/auth/auth.go
+++ backend/auth_service/internal/http-server/handler/auth/auth.go
@@ -1,1 +1,11 @@
 package auth
+
+import "context"
+
+type Authorization interface {
+	Login(ctx context.Context, email string, password string) (err error)
+	Register(ctx context.Context, email string, username string, password string) (err error)
+	Logout(refresh_token)
+	RefreshToken(refresh_token)
+	VerifyEmail(code)
+}
