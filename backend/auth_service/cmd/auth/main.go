fc959eea55effa63cd12603810dd3d047d10279b
stepan41k <russia.zont@gmail.com>
2026-03-09 22:25:32 +0300

    feat: add endpoints to router

--- backend/auth_service/cmd/auth/main.go
+++ backend/auth_service/cmd/auth/main.go
@@ -12,6 +12,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/sudo-odner/min/backend/auth-service/internal/app"
 	"github.com/sudo-odner/min/backend/auth-service/internal/config"
+	authHandler "github.com/sudo-odner/min/backend/auth-service/internal/http-server/handler/auth"
+	authService "github.com/sudo-odner/min/backend/auth-service/internal/service/auth"
 	"github.com/sudo-odner/min/backend/auth-service/internal/http-server/middleware/cors"
 	"github.com/sudo-odner/min/backend/auth-service/internal/repository/postgres"
 	"go.uber.org/zap"
@@ -35,6 +37,9 @@ func main() {
 
 	log.Info("starting authentication service")
 
+	authService := authService.New(dbConn, log)
+	authHandler := authHandler.New(authService, log)
+
 	router := chi.NewRouter()
 
 	router.Use(middleware.RequestID)
@@ -43,8 +48,13 @@ func main() {
 	router.Use(middleware.URLFormat)
 
 	router.Route("/auth-service", func(r chi.Router) {
-		// r.Post("/refresh")
+		r.Post("/register", authHandler.Register(context.Background()))
+		r.Post("/login", authHandler.Login(context.Background()))
 	})
+
+	// router.Route("/token", func(r chi.Router) {
+	// 	r.Post("/refresh")
+	// })
 
 	application := app.New(log, cfg, router)
 
