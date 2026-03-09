22f3594c2d5569014aa6d17b5b129e4c066c509a
stepan41k <russia.zont@gmail.com>
2026-03-09 22:29:13 +0300

    feat: add simple implmentation of authentication service layer

--- backend/auth_service/internal/service/auth/auth.go
+++ backend/auth_service/internal/service/auth/auth.go
@@ -1,17 +1,74 @@
 package auth
 
-import "context"
+import (
+	"context"
+	"fmt"
 
-type AuthRepository interface {
-	Login(ctx context.Context, email string, password string) (Tokens, error)
-	Register(ctx context.Context, input SignUpInput) error
-	Logout(refresh_token)
-	VerifyEmail(ctx context.Context, code string) error
+	"github.com/google/uuid"
+	"github.com/sudo-odner/min/backend/auth-service/internal/config"
+	"github.com/sudo-odner/min/backend/auth-service/internal/lib/jwt"
+	"github.com/sudo-odner/min/backend/auth-service/internal/models"
+	"go.uber.org/zap"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type AuthorizationRepository interface {
+	Create(ctx context.Context, input models.User) (error)
+	GetByEmail(ctx context.Context, email string) (*models.User, error)
+	GetByID(ctx context.Context, id uuid.UUID) (*models.User, error)
+	// VerifyEmail(ctx context.Context, code string) error
 }
 
-type AuthService struct {
-	
+type AuthorizationService struct {
+	authRepository AuthorizationRepository
+	log *zap.Logger
+	tokenConfig config.TokenConfig
 }
 
+func New(authRepository AuthorizationRepository, log *zap.Logger) *AuthorizationService {
+	return &AuthorizationService{
+		authRepository: authRepository,
+		log: log,
+	}
+}
+
+func (as *AuthorizationService) Login(ctx context.Context, email string, password string) (user *models.User, accessToken string, refreshToken string, err error) {
+	const op = "service.auth.Login"
+
+	log := as.log.With(
+		zap.String("op", op),
+	)
+
+	log.Info("attempting to login user")
+
+	user, err = as.authRepository.GetByEmail(ctx, email)
+	if err != nil {
+		return nil, "", "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(password))
+	if err != nil {
+		return nil, "", "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	accessToken, refreshToken, err = jwt.GenerateTokens(as.tokenConfig, user.ID, user.Email)
+	if err != nil {
+		return nil, "", "", fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, accessToken, refreshToken, nil
+}
+
+func (as *AuthorizationService) Register(ctx context.Context, email string, username string, password string) (user *models.User, accessToken string, refreshToken string, err error) {
+	const op = "service.auth.Register"
+
+	log := as.log.With(
+		zap.String("op", op),
+	)
+
+	log.Info("attempting to register new user")
+
+	return
+}
 
 
