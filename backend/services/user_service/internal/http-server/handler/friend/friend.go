da66b9c6288fe0f06286979b79f84a03197f6788
sudo-odner <odner.main@gmail.com>
2026-03-15 20:39:19 +0300

    feat(user_service): write base handler && inteface service

--- backend/services/user_service/internal/http-server/handler/friend/friend.go
+++ backend/services/user_service/internal/http-server/handler/friend/friend.go
@@ -1,1 +1,62 @@
 package friend
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/sudo-odner/minor/backend/services/user_service/internal/model"
+	"go.uber.org/zap"
+)
+
+type FriendService interface {
+	SendFriendRequest(ctx context.Context, userID, friendID uuid.UUID) error                 // Запрос на дружбу
+	FriendList(ctx context.Context, userID uuid.UUID) ([]*model.FriendPreview, error)        // Получить список друзей
+	FriendRequestList(ctx context.Context, userID uuid.UUID) ([]*model.FriendPreview, error) // Получить список статусов запросов на дружбу
+	AcceptFriendRequest(ctx context.Context, actorID, targetID uuid.UUID) error              // Принять запрос
+	DenyFriendRequest(ctx context.Context, actorID, targetID uuid.UUID) error                // Отклонить запрос
+	BlockUser(ctx context.Context, actorID, target uuid.UUID) error                          // Заблокировать
+	RemoveFriend(ctx context.Context, actorID, targerID uuid.UUID) error                     // Удалить друга
+}
+
+type FriendHandler struct {
+	log           *zap.Logger
+	friendService FriendService
+}
+
+func New(log *zap.Logger, friendService FriendService) *FriendHandler {
+	return &FriendHandler{
+		log:           log,
+		friendService: friendService,
+	}
+}
+
+func (h *FriendHandler) SendFriendRequest(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *FriendHandler) FriendList(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *FriendHandler) FriendRequestList(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *FriendHandler) AnswerFriendRequest(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *FriendHandler) DeleteFriendship(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
--- backend/services/user_service/internal/http-server/handler/user/user.go
+++ backend/services/user_service/internal/http-server/handler/user/user.go
@@ -1,1 +1,53 @@
 package user
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/sudo-odner/minor/backend/services/user_service/internal/model"
+	"go.uber.org/zap"
+)
+
+type UserService interface {
+	CreateUser(ctx context.Context, userID uuid.UUID, username, bio string) (*model.User, error) // Создание пользователя
+	GetUser(ctx context.Context, userID uuid.UUID) (*model.User, error)                          // Получение профиля пользователя по ID
+	UpdateUser(ctx context.Context, username, bio *string) (*model.User, error)                  // Изменнеие пользователя
+	DeleteUser(ctx context.Context, userID uuid.UUID) error                                      // Удаление пользователя
+}
+
+type UserHandler struct {
+	log         *zap.Logger
+	userService UserService
+}
+
+func New(log *zap.Logger, userService UserService) *UserHandler {
+	return &UserHandler{
+		log:         log,
+		userService: userService,
+	}
+}
+
+func (h *UserHandler) CreateUser(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *UserHandler) GetUser(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *UserHandler) UpdateUser(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
+
+func (h *UserHandler) DeleteUser(ctx context.Context) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+	}
+}
