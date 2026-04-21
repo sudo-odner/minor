3bd62f5154d6e2f9a6bef3372351e143256b986d
sudo-odner <odner.main@gmail.com>
2026-04-21 20:58:05 +0300

    feat: add config

--- backend/services/chat_service/cmd/chat/main.go
+++ backend/services/chat_service/cmd/chat/main.go
@@ -1,1 +1,9 @@
 package main
+
+import (
+	"github.com/sudo-odner/minor/backend/services/chat_service/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+}
---
+++ backend/services/chat_service/internal/config/config.go
@@ -0,0 +1,44 @@
+package config
+
+import (
+	"log"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	Env        string `env:"ENV" env-required:"true"`
+	HttpServer HttpServer
+	Cassandra  Cassandra
+	Nuts       Nuts
+}
+
+type HttpServer struct {
+	Address     string        `env:"HTTP_SERVER_ADDRESS" env-required:"true"`
+	Timeout     time.Duration `env:"HTTP_SERVER_TIMEOUT"`
+	IdleTimeout time.Duration `env:"HTTP_SERVER_IDLE_TIMEOUT"`
+}
+
+type Cassandra struct {
+	Url string `env:"CASSANDRA_URL" env-required:"true"`
+}
+
+type Nuts struct {
+	Url string `env:"NATS_URL" env-required:"true"`
+}
+
+func MustLoad() *Config {
+	if err := godotenv.Load(); err != nil {
+		log.Println("DEBUG: not found .env file, read form env")
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadEnv(&cfg); err != nil {
+		log.Fatalf("cannot read config: %s", err)
+	}
+
+	return &cfg
+}
