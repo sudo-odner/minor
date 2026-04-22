ad2cfe463df7a75b19d2140ba3d82b5db45d5aaf
sudo-odner <odner.main@gmail.com>
2026-04-22 17:24:48 +0300

    feat(chat-service): add domain model NewMessage

--- backend/services/chat_service/internal/models/domain.go
+++ backend/services/chat_service/internal/models/domain.go
@@ -2,6 +2,13 @@ package models
 
 import "time"
 
+type NewMessage struct {
+	ChannelID []byte
+	AuthorID  []byte
+	Content   string
+	ReplyTo   []byte
+}
+
 type Message struct {
 	ChannelID []byte
 	MessageID []byte
