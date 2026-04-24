007ddac6946b2b05030ee950e7ab35f28b0d7669
sudo-odner <odner.main@gmail.com>
2026-04-24 15:37:28 +0300

    feat(chat-service): implement publish message is created(PublishMessageCreated)

--- backend/services/chat_service/internal/broker/nuts/messages.go
+++ backend/services/chat_service/internal/broker/nuts/messages.go
@@ -2,12 +2,37 @@ package nuts
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/sudo-odner/minor/backend/services/chat_service/internal/models"
 )
 
 func (b *Broker) PublishMessageCreated(ctx context.Context, msg *models.Message) error {
+	const op = "broker.nuts.PublishMessageCreated"
+
+	event := MessageCreatedEvent{
+		MessageID: msg.MessageID.String(),
+		ChannelID: msg.ChannelID.String(),
+		AuthorID:  msg.AuthorID.String(),
+		Content:   msg.Content,
+		CreatedAt: msg.CreatedAt,
+	}
+	if msg.ReplyTo != nil {
+		replyTo := msg.ReplyTo.String()
+		event.ReplyTo = &replyTo
+	}
+
+	data, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := b.conn.Publish(SubjectMessageCreated, data); err != nil {
+		return fmt.Errorf("%s: message not publish: %w", op, err)
+	}
+
 	return nil
 }
 
