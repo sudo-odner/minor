704a61dcd8e3ce572a77eb1e2f639d476767e6fc
sudo-odner <odner.main@gmail.com>
2026-04-24 15:51:30 +0300

    feat(chat-service): implement publish message is deleted (PbulishMEssageDeleted)

--- backend/services/chat_service/internal/broker/nuts/messages.go
+++ backend/services/chat_service/internal/broker/nuts/messages.go
@@ -37,5 +37,21 @@ func (b *Broker) PublishMessageCreated(ctx context.Context, msg *models.Message)
 }
 
 func (b *Broker) PublishMessageDeleted(ctx context.Context, channelID, messageID uuid.UUID) error {
+	const op = "broker.nuts.PublishMessageDeleted"
+
+	event := MessageDeletedEvent{
+		MessageID: messageID.String(),
+		ChannelID: channelID.String(),
+	}
+
+	data, err := json.Marshal(event)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	if err := b.conn.Publish(SubjectMessageDeleted, data); err != nil {
+		return fmt.Errorf("%s: failed publish message: %w", op, err)
+	}
+
 	return nil
 }
