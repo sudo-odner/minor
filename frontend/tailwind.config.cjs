f7347e4299b98862fe848e3b4461e3d89cc807a2
stepan41k <russia.zont@gmail.com>
2026-05-10 13:13:40 +0300

    feat: initialize simple frontend preview

---
+++ frontend/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                'klein-blue': '#002FA7',
+            },
+        },
+    },
+    plugins: [],
+};
