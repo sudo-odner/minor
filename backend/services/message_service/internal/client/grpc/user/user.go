84de686f4a2d27f32b3c3132f98db8c632b2ea22
sudo-odner <odner.main@gmail.com>
2026-05-22 17:09:15 +0300

    feat(message_service): write base grpc client for user

---
+++ backend/services/message_service/internal/client/grpc/user/user.go
@@ -0,0 +1,45 @@
+package user
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/google/uuid"
+	guildv1 "github.com/sudo-odner/minor/backend/shared/pkg/pb/guild/v1"
+	userv1 "github.com/sudo-odner/minor/backend/shared/pkg/pb/guild/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type Clinet struct {
+	client userv1.GuildServiceClient
+	conn   *grpc.ClientConn
+}
+
+func New(target string) (*Clinet, error) {
+	const op = "client.grpc.user.New"
+
+	conn, err := grpc.NewClient(target, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return &Clinet{
+		client: guildv1.NewGuildServiceClient(conn),
+		conn:   conn,
+	}, nil
+}
+
+func (c *Clinet) Close() error {
+	return c.conn.Close()
+}
+
+func (c *Clinet) CanWrite(ctx context.Context, userID, channelID uuid.UUID) (bool, error) {
+	// TODO: Implement
+	return true, nil
+}
+
+func (c *Clinet) CanRead(ctx context.Context, userID, channelID uuid.UUID) (bool, error) {
+	// TODO: Implement
+	return true, nil
+}
