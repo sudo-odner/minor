70cacbbd6ddf9386f4fdb2b9e95b77be8c4a45d2
sudo-odner <odner.main@gmail.com>
2026-05-24 16:05:53 +0300

    fix(message_service): fix naming cfg and .env

--- backend/services/message_service/internal/config/config.go
+++ backend/services/message_service/internal/config/config.go
@@ -49,7 +49,7 @@ type GRPC struct {
 }
 
 type GRPCClient struct {
-	TargetUser      string `env:"GRPC_CLINET_USER_TARGET" env-required:"true"`
+	TargetUser      string `env:"GRPC_CLIENT_USER_TARGET" env-required:"true"`
 	TargetCommunity string `env:"GRPC_CLIENT_COMMUNITY_TARGET" env-required:"true"`
 }
 
