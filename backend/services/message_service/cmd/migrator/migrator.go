ebce8812ada4e7b7cc81b4e26c7ebe49b3de8643
sudo-odner <odner.main@gmail.com>
2026-05-24 20:25:25 +0300

    feat(message_service): add migrator on cassandra

---
+++ backend/services/message_service/cmd/migrator/migrator.go
@@ -0,0 +1,32 @@
+package migrator
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+
+	"github.com/sudo-odner/minor/backend/services/message_service/internal/migrator"
+)
+
+func main() {
+	const op = "cmd.migrator"
+
+	log.Printf("[%s] Starting migrator...", op)
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	dbHosts := os.Getenv("CASSANDRA_HOSTS")
+	if dbHosts == "" {
+		dbHosts = "127.0.0.1"
+	}
+	hosts := strings.Split(dbHosts, ",")
+
+	if err := migrator.RunMigrations(ctx, hosts, "chat"); err != nil {
+		log.Printf("[%s] Migrator stoped with error: %v", op, err)
+		return
+	}
+	log.Printf("[%s] Migrator successful created", op)
+}
