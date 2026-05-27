d346191fa24ab82c536c15cc55c926a71b2362c5
sudo-odner <odner.main@gmail.com>
2026-05-27 20:37:22 +0300

    feat(community-service): init repository postgres

--- backend/services/community_service/internal/repository/postgres/postgres.go
+++ backend/services/community_service/internal/repository/postgres/postgres.go
@@ -1,17 +1,44 @@
 package postgres
 
-import "context"
+import (
+	"context"
+	"fmt"
 
-type Repository struct{}
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
-func New() (*Repository, error) {
-	return nil, nil
+type Repository struct {
+	pool *pgxpool.Pool
+}
+
+func New(ctx context.Context, dsn string) (*Repository, error) {
+	const op = "repository.postgres.New"
+
+	pool, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+	if err := pool.Ping(ctx); err != nil {
+		return nil, fmt.Errorf("%s: ping falied: %w", op, err)
+	}
+
+	return &Repository{
+		pool: pool,
+	}, nil
 }
 
 func (repo *Repository) Ping(ctx context.Context) error {
+	if repo.pool != nil {
+		if err := repo.pool.Ping(ctx); err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
 func (repo *Repository) Close(ctx context.Context) error {
+	if repo.pool != nil {
+		repo.pool.Close()
+	}
 	return nil
 }
