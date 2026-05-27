920738518a8f8c2dde850a6857cd3d8e1bbf91bf
sudo-odner <odner.main@gmail.com>
2026-05-27 13:25:11 +0300

    feat(community-service): add TODO

--- backend/services/community_service/cmd/community/main.go
+++ backend/services/community_service/cmd/community/main.go
@@ -1,1 +1,9 @@
 package main
+
+func main() {
+	// TODO: Init cofnig
+	// TODO: Init logger
+	// TODO: Init application
+
+	// TODO: Run and Stop app with graceful shutdown
+}
--- backend/services/community_service/internal/app/grpc/grpc.go
+++ backend/services/community_service/internal/app/grpc/grpc.go
@@ -1,1 +1,27 @@
 package grpcServ
+
+import "context"
+
+type App struct {
+	log        any
+	reposipory any
+	broker     any
+	httpServer any
+	grpcServer any
+}
+
+func New() *App {
+	const op = "app.New"
+	// TODO: Init repository Postgres
+	// TODO: Init broker Nuts
+	// TODO: Init service
+	// TODO: Init HTTP server
+	// TODO: Init gRPC server
+	return nil
+}
+
+func (a *App) Run() {
+}
+
+func (a *App) Stop(ctx context.Context) {
+}
