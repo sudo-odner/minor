f13fee560069e14ad97ae06a13a5b719477a03a5
sudo-odner <odner.main@gmail.com>
2026-05-28 19:30:56 +0300

    feat(community-service): create add, get, gets member server

--- backend/services/community_service/internal/service/members/service.go
+++ backend/services/community_service/internal/service/members/service.go
@@ -2,6 +2,7 @@ package members
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/google/uuid"
 	"github.com/sudo-odner/minor-shared/pkg/authz"
@@ -10,7 +11,7 @@ import (
 )
 
 type Repository interface {
-	AddMember(ctx context.Context, member *models.Member) (*models.Member, error)
+	AddMember(ctx context.Context, serverID uuid.UUID, userID uuid.UUID, nickname *string) (*models.Member, error)
 	GetServerMember(ctx context.Context, serverID, userID uuid.UUID) (*models.Member, error)
 	GetServerMembers(ctx context.Context, serverID uuid.UUID) ([]models.Member, error)
 	RemoveMember(ctx context.Context, serverID, userID uuid.UUID) error
@@ -22,15 +23,48 @@ type PermissionService interface {
 	FetchServerPermissions(ctx context.Context, userID, serverID uuid.UUID) (authz.Permission, error)
 }
 type Service struct {
-	log         *zap.Logger
-	repo        Repository
-	permService PermissionService
+	log  *zap.Logger
+	repo Repository
+	perm PermissionService
 }
 
 func New(log *zap.Logger, repo Repository, permService PermissionService) *Service {
 	return &Service{
-		log:         log,
-		repo:        repo,
-		permService: permService,
+		log:  log,
+		repo: repo,
+		perm: permService,
 	}
+}
+
+func (s *Service) AddMember(ctx context.Context, serverID, userID uuid.UUID, nickname *string) (*models.Member, error) {
+	const op = "service.member.AddMember"
+
+	m, err := s.repo.AddMember(ctx, serverID, userID, nickname)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return m, nil
+}
+
+func (s *Service) GetServerMember(ctx context.Context, serverID, userID uuid.UUID) (*models.Member, error) {
+	const op = "service.memver.GetServerMemver"
+
+	m, err := s.repo.GetServerMember(ctx, serverID, userID)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return m, nil
+}
+
+func (s *Service) GetServerMembers(ctx context.Context, serverID uuid.UUID) ([]models.Member, error) {
+	const op = "service.member.GetServerMembers"
+
+	ms, err := s.repo.GetServerMembers(ctx, serverID)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return ms, nil
 }
