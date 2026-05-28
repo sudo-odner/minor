3d43575c8e7ba198d7b18ce184cf4641365efe34
sudo-odner <odner.main@gmail.com>
2026-05-28 18:04:35 +0300

    feat(community-service): write base service 'server'

---
+++ backend/services/community_service/internal/service/servers/servers.go
@@ -0,0 +1,51 @@
+package servers
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+	"github.com/sudo-odner/minor/backend/services/community_service/internal/models"
+	"go.uber.org/zap"
+)
+
+type Repository interface {
+	CreateServerWithDefaultSetup(ctx context.Context, name string, ownerID uuid.UUID, avatarURL string) (*models.Server, error)
+	GetServer(ctx context.Context, serverID uuid.UUID) (*models.Server, error)
+	UpdateServer(ctx context.Context, serverID uuid.UUID, name *string, avatarURL *string) (*models.Server, error)
+	DeleteServer(ctx context.Context, serverID uuid.UUID) error
+}
+
+type Service struct {
+	log  *zap.Logger
+	repo Repository
+}
+
+func New(log *zap.Logger, repo Repository) *Service {
+	return &Service{
+		log:  log,
+		repo: repo,
+	}
+}
+
+// AvatarURL не работает на данный момент всегда идет записть "" так как нет сервса который хранит картинки
+// TODO: Написать Events под манипуляциями над сервером, чтобы обновлялось у всех
+
+func (s *Service) CreateServer(ctx context.Context, name string, ownerID uuid.UUID, avatarURL string) (*models.Server, error) {
+	const op = "service.server.CreateServer"
+	return nil, nil
+}
+
+func (s *Service) GetServer(ctx context.Context, serverID uuid.UUID) (*models.Server, error) {
+	const op = "service.server.GetServer"
+	return nil, nil
+}
+
+func (s *Service) UpdateServer(ctx context.Context, serverID uuid.UUID, name *string, avatarURL *string) (*models.Server, error) {
+	const op = "service.server.UpdateServer"
+	return nil, nil
+}
+
+func (s *Service) DeleteServer(ctx context.Context, serverID uuid.UUID) error {
+	const op = "service.server.DeleteServer"
+	return nil
+}
