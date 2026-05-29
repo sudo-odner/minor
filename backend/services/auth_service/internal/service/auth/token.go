354b8557e96f132f805416439a347c8c324e7b14
stepan41k <russia.zont@gmail.com>
2026-05-29 22:49:10 +0300

    refactor(auth): update token logic

--- backend/services/auth_service/internal/service/auth/token.go
+++ backend/services/auth_service/internal/service/auth/token.go
@@ -1,30 +1,21 @@
 package auth
 
 import (
-	"fmt"
-	"github.com/golang-jwt/jwt/v5"
+	"context"
+	"errors"
+
+	"github.com/sudo-odner/minor/backend/services/auth_service/internal/lib/jwt"
+	"github.com/sudo-odner/minor/backend/services/auth_service/internal/models"
 )
 
-type UserClaims struct {
-	UserID string `json:"user_id"`
-	jwt.RegisteredClaims
-}
-
-func (s *AuthorizationService) VerifyAccessToken(tokenStr string) (*UserClaims, error) {
-	token, err := jwt.ParseWithClaims(tokenStr, &UserClaims{}, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
-		}
-		return []byte(s.tokenConfig.AccessSecret), nil
-	})
-
+func (s *AuthService) VerifyAccessToken(ctx context.Context, tokenStr string) (*models.Claims, error) {
+	claims, err := jwt.ValidateAccessToken(s.authConfig, tokenStr)
 	if err != nil {
-		return nil, err
+		if errors.Is(err, jwt.ErrExpiredToken) {
+			return nil, ErrExpiredToken
+		}
+		return nil, ErrInvalidToken
 	}
-
-	if claims, ok := token.Claims.(*UserClaims); ok && token.Valid {
-		return claims, nil
-	}
-
-	return nil, fmt.Errorf("invalid token")
+    
+	return claims, nil
 }
