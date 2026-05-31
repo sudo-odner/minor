6d9b1defab2e1eca06286ff6c8bf1e59a6dc3137
sudo-odner <odner.main@gmail.com>
2026-05-31 16:29:28 +0300

    feat(presence-service): write base service

--- backend/services/presence_service/internal/service/presence/service.go
+++ backend/services/presence_service/internal/service/presence/service.go
@@ -1,1 +1,23 @@
 package presence
+
+import (
+	"go.uber.org/zap"
+)
+
+type Cache interface{}
+
+type Broker interface{}
+
+type Service struct {
+	log    *zap.Logger
+	cache  Cache
+	broker Broker
+}
+
+func New(log *zap.Logger, cache Cache, broker Broker) *Service {
+	return &Service{
+		log:    log,
+		cache:  cache,
+		broker: broker,
+	}
+}
