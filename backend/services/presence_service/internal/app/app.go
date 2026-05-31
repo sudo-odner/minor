6f138865bd1fad493119f1992abdf5a00c7874a6
sudo-odner <odner.main@gmail.com>
2026-05-31 15:20:29 +0300

    chore(presence-service): delete old struct(auth-services) and make base struct for presence-service

--- backend/services/presence_service/internal/app/app.go
+++ backend/services/presence_service/internal/app/app.go
@@ -1,22 +1,16 @@
 package app
 
 import (
-	"github.com/go-chi/chi/v5"
-	httpapp "github.com/sudo-odner/minor/backend/services/presence_service/internal/app/http"
 	"github.com/sudo-odner/minor/backend/services/presence_service/internal/config"
 	"go.uber.org/zap"
 )
 
 type App struct {
-	HTTPServer *httpapp.App
-	log        *zap.Logger
+	log *zap.Logger
 }
 
-func New(log *zap.Logger, cfg *config.Config, router chi.Router) *App {
-	httpApp := httpapp.New(log, cfg, router)
-
+func New(cfg *config.Config, log *zap.Logger) *App {
 	return &App{
-		HTTPServer: httpApp,
-		log:        log,
+		log: log,
 	}
 }
