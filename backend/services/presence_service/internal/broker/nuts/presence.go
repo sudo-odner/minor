859c5419d48a5d38db5daa3e44d88cf44af76db4
sudo-odner <odner.main@gmail.com>
2026-05-31 16:36:33 +0300

    feat(presence-service): init struct broker

---
+++ backend/services/presence_service/internal/broker/nuts/presence.go
@@ -0,0 +1,10 @@
+package nuts
+
+import (
+	"context"
+)
+
+func (b *Broker) PublishPresenceStatusUpdated(ctx context.Context) error {
+	const op = "broker.nuts.PublishPresenceStatusUpdated"
+	return nil
+}
