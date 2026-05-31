87186b8655be4b3d1ae4de0952637d3dfa88a049
sudo-odner <odner.main@gmail.com>
2026-05-31 16:42:50 +0300

    feat(presence-service): write grpc

--- backend/services/presence_service/internal/server/grpc/presence.go
+++ backend/services/presence_service/internal/server/grpc/presence.go
@@ -4,6 +4,8 @@ import (
 	"context"
 
 	presencev1 "github.com/sudo-odner/minor-shared/pkg/pb/presence/v1"
+	"github.com/sudo-odner/minor/backend/services/presence_service/internal/models"
+	"go.uber.org/zap"
 )
 
 func (s *ServerAPI) SetStatus(
@@ -11,7 +13,17 @@ func (s *ServerAPI) SetStatus(
 	req *presencev1.SetStatusRequest,
 ) (*presencev1.SetStatusResponse, error) {
 	const op = "server.grpc.SetStatus"
-	// TODO: implement me
+
+	userID := req.GetUserId()
+	status := models.UserStatus(req.GetStatus())
+	statusInfo := req.GetStatusInfo()
+
+	err := s.presenceService.SetStatus(ctx, userID, status, statusInfo)
+	if err != nil {
+		s.log.Error("failed to set status", zap.String("op", op), zap.Error(err))
+		return &presencev1.SetStatusResponse{Success: false}, err
+	}
+
 	return &presencev1.SetStatusResponse{
 		Success: true,
 	}, nil
@@ -21,9 +33,26 @@ func (s *ServerAPI) GetUserStatuses(
 	ctx context.Context,
 	req *presencev1.GetUserStatusesRequest,
 ) (*presencev1.GetUserStatusesResponse, error) {
-	const op = "server.grpc.GetUserStatues"
-	// TODO: implement me
+	const op = "server.grpc.GetUserStatuses"
+
+	userIDs := req.GetUserIds()
+	res, err := s.presenceService.GetUserStatuses(ctx, userIDs)
+	if err != nil {
+		s.log.Error("failed to get user statuses", zap.String("op", op), zap.Error(err))
+		return nil, err
+	}
+
+	pbStatuses := make(map[string]*presencev1.Presence)
+	for id, p := range res {
+		pbStatuses[id] = &presencev1.Presence{
+			UserId:       p.UserID,
+			Status:       presencev1.UserStatus(p.Status),
+			CustomStatus: p.CustomStatus,
+			LastActiveAt: p.LastActiveAt,
+		}
+	}
+
 	return &presencev1.GetUserStatusesResponse{
-		Statuses: nil,
+		Statuses: pbStatuses,
 	}, nil
 }
