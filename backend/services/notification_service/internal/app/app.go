87949179ab3f33c79671fd2997139b930b740db9
stepan41k <russia.zont@gmail.com>
2026-06-01 18:12:37 +0300

    chore(notification): comment app.go

--- backend/services/notification_service/internal/app/app.go
+++ backend/services/notification_service/internal/app/app.go
@@ -1,21 +1,21 @@
 package app
 
-import (
-	httpapp "github.com/sudo-odner/minor/backend/services/notification_service/internal/app/http"
-	"github.com/sudo-odner/minor/backend/services/notification_service/internal/config"
-	"go.uber.org/zap"
-)
+// import (
+// 	httpapp "github.com/sudo-odner/minor/backend/services/notification_service/internal/app/http"
+// 	"github.com/sudo-odner/minor/backend/services/notification_service/internal/config"
+// 	"go.uber.org/zap"
+// )
 
-type App struct {
-	HTTPServer *httpapp.App
-	log        *zap.Logger
-}
+// type App struct {
+// 	HTTPServer *httpapp.App
+// 	log        *zap.Logger
+// }
 
-func New(log *zap.Logger, cfg *config.Config) *App {
-	httpApp := httpapp.New(log, cfg)
+// func New(log *zap.Logger, cfg *config.Config) *App {
+// 	httpApp := httpapp.New(log, cfg)
 
-	return &App{
-		HTTPServer: httpApp,
-		log:        log,
-	}
-}
+// 	return &App{
+// 		HTTPServer: httpApp,
+// 		log:        log,
+// 	}
+// }
