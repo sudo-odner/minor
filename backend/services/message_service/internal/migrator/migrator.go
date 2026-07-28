a08f2c244e51d610da91c0cdb3fb5bad37908091
sudo-odner <odner.main@gmail.com>
2026-07-28 19:56:10 +0300

    feat(message_service/migrator): rewrite migrator, make on gocqlx

--- backend/services/message_service/internal/migrator/migrator.go
+++ backend/services/message_service/internal/migrator/migrator.go
@@ -5,107 +5,91 @@ import (
 	"embed"
 	"fmt"
 	"log"
-	"strings"
 	"time"
 
 	"github.com/gocql/gocql"
+	"github.com/scylladb/gocqlx/v3"
+	"github.com/scylladb/gocqlx/v3/migrate"
 )
 
 //go:embed migrations/*.cql
 var migrationFiles embed.FS
 
-func reconnect(ctx context.Context, clusterCfg *gocql.ClusterConfig, maxReconect int) (*gocql.Session, error) {
+func reconnect(ctx context.Context, clusterCfg *gocql.ClusterConfig, maxRetries int, retryInterval time.Duration) (*gocql.Session, error) {
 	const op = "migrator.reconnect"
-	var systemSession *gocql.Session
-	var err error
 
-	systemSession, err = clusterCfg.CreateSession()
-	if err == nil {
-		return systemSession, err
-	}
-
-	ticker := time.NewTicker(3 * time.Second)
+	ticker := time.NewTicker(retryInterval)
 	defer ticker.Stop()
-	for i := 0; i < maxReconect; i++ {
+
+	for try := 1; try <= maxRetries; try++ {
 		select {
 		case <-ctx.Done():
-			return nil, fmt.Errorf("%s: migrator cancel by context: %w", op, ctx.Err())
+			return nil, fmt.Errorf("%s: canceled by context: %w", op, ctx.Err())
+		default:
+		}
+
+		session, err := clusterCfg.CreateSession()
+		if err == nil {
+			return session, nil
+		}
+
+		log.Printf("%s: database not answering (try %d/%d)", op, try, maxRetries)
+		if try == maxRetries {
+			break
+		}
+
+		select {
+		case <-ctx.Done():
+			return nil, fmt.Errorf("%s: canceled by context: %w", op, ctx.Err())
 		case <-ticker.C:
-			systemSession, err = clusterCfg.CreateSession()
-			if err == nil {
-				return systemSession, nil
-			}
-			log.Printf("%s: database not answer (try %d/%d)", op, i, maxReconect)
 		}
 	}
-	return nil, fmt.Errorf("%s: connection attempt limit exceeded: %w", op, err)
+	return nil, fmt.Errorf("%s: connection limit exceeded (%d attempts)", op, maxRetries)
 }
 
-func RunMigrations(ctx context.Context, hosts []string, keyspace string) error {
+// TODO: Better create config settings for migrations (for setting keyspace)
+func RunMigrations(ctx context.Context, hosts []string, keyspace string, maxRetries int, retryInterval time.Duration) error {
 	const op = "migrator.RunMigrations"
-
-	// 0. Start setting cluster
+	// Init cluster config
 	cluster := gocql.NewCluster(hosts...)
-	cluster.Consistency = gocql.Quorum
 	cluster.Timeout = 5 * time.Second
 
-	// 1. Connect to Database
-	systemSession, err := reconnect(ctx, cluster, 15)
+	// Connect to cluster cassandra and create keyspcase
+	err := func() error {
+		session, err := reconnect(ctx, cluster, maxRetries, retryInterval)
+		if err != nil {
+			return err
+		}
+		defer session.Close()
+
+		log.Printf("%s: success connect to Cassandra", op)
+
+		createKeySpaceQuery := fmt.Sprintf(`
+			CREATE KEYSPACE IF NOT EXISTS %s
+			WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
+		`, keyspace)
+		if err := session.Query(createKeySpaceQuery).Exec(); err != nil {
+			return fmt.Errorf("failed create keyspace: %w", err)
+		}
+		log.Printf("%s: keyspace '%s' is created/checked", op, keyspace)
+		return nil
+	}()
 	if err != nil {
 		return fmt.Errorf("%s: %w", op, err)
 	}
-	defer systemSession.Close()
-	log.Printf("%s: success connect to Cassandra", op)
 
-	// 2. Creating keyspace
-	createKeySpace := fmt.Sprintf(`
-		CREATE KEYSPACE IF NOT EXISTS %s
-		WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 1};
-	`, keyspace)
-
-	if err := systemSession.Query(createKeySpace).Exec(); err != nil {
-		return fmt.Errorf("%s: failed create keyspace: %w", op, err)
-	}
-	systemSession.Close()
-	log.Printf("%s: keyspace '%s' is created", op, keyspace)
-
-	// 3. Migrate migrations
+	// Connect to cluser with keyspace and run migrations
 	cluster.Keyspace = keyspace
-	appSession, err := cluster.CreateSession()
+	cqlxSession, err := gocqlx.WrapSession(reconnect(ctx, cluster, maxRetries, retryInterval))
 	if err != nil {
 		return fmt.Errorf("%s: failed connect to keyspace: %w", op, err)
 	}
-	defer appSession.Close()
+	defer cqlxSession.Close()
 
-	entries, err := migrationFiles.ReadDir("migrations")
-	if err != nil {
-		return fmt.Errorf("%s: falied to read the embedded migrations folder: %w", op, err)
+	log.Printf("%s: running migration on keyspace '%s'...", op, keyspace)
+	if err := migrate.FromFS(ctx, cqlxSession, migrationFiles); err != nil {
+		return fmt.Errorf("%s: falied migrate: %w", op, err)
 	}
-
-	for _, entry := range entries {
-		if entry.IsDir() || !strings.HasSuffix(entry.Name(), ".cql") {
-			continue
-		}
-
-		content, err := migrationFiles.ReadFile("migrations/" + entry.Name())
-		if err != nil {
-			return fmt.Errorf("%s: falied to read '%s' file: %w", op, entry.Name(), err)
-		}
-		log.Printf("%s: exec file %s", op, entry.Name())
-
-		// Cassandra не умеет выполнять пачку DDL-запросов за раз
-		// Разбиваем запросы по ';' и поледовательно выполняем запросы
-		for query := range strings.SplitSeq(string(content), ";") {
-			clearQuery := strings.TrimSpace(query)
-			if clearQuery == "" {
-				continue
-			}
-
-			if err := appSession.Query(clearQuery).Exec(); err != nil {
-				return fmt.Errorf("%s: error in file '%s' on query [%s]: %w", op, entry.Name(), query, err)
-			}
-		}
-	}
-	log.Printf("%s: database schema successful complete", op)
+	log.Printf("%s: database schema successufully update", op)
 	return nil
 }
