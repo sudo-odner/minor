bbda24feafd9da67f75ea15f6858b02efb855b86
sudo-odner <odner.main@gmail.com>
2026-09-08 14:21:27 +0300

    fear(service/dm): write migrations files

---
+++ backend/services/dm_service/internal/repository/postgres/postgres.go
@@ -0,0 +1,15 @@
+package postgres
+
+import (
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+type Repository struct {
+	pool *pgxpool.Pool
+}
+
+func New(pool *pgxpool.Pool) *Repository {
+	return &Repository{
+		pool: pool,
+	}
+}
