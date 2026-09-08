d269a66759469e7ebc9d2d219b56ecf27ebda69a
sudo-odner <odner.main@gmail.com>
2026-09-08 14:29:18 +0300

    feat(service/dm): write init postgres and config

--- backend/services/dm_service/internal/app/app.go
+++ backend/services/dm_service/internal/app/app.go
@@ -2,15 +2,34 @@ package app
 
 import (
 	"context"
+	"fmt"
 	"log/slog"
 
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/sudo-odner/minor/backend/service/dm_service/internal/config"
+	"github.com/sudo-odner/minor/backend/service/dm_service/internal/repository/postgres"
 )
 
-type App struct{}
+type App struct {
+	log *slog.Logger
+
+	postgrespool *pgxpool.Pool
+}
 
 func New(cfg *config.Config, log *slog.Logger) (*App, error) {
-	return nil, nil
+	const op = "app.New"
+	ctx := context.Background()
+	a := &App{log: log}
+
+	// Init repository
+	pool, err := pgxpool.New(ctx, cfg.Postgres.ConnString())
+	if err != nil {
+		return nil, fmt.Errorf("%s: falied connect to postgres: %w", op, err)
+	}
+	a.postgrespool = pool
+	_ = postgres.New(pool)
+
+	return a, nil
 }
 
 func (a *App) Run() error {
@@ -18,5 +37,8 @@ func (a *App) Run() error {
 }
 
 func (a *App) Stop(ctx context.Context) error {
+	if a.postgrespool != nil {
+		a.postgrespool.Close()
+	}
 	return nil
 }
--- backend/services/dm_service/internal/config/config.go
+++ backend/services/dm_service/internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"fmt"
 	"log"
 	"time"
 
@@ -10,12 +11,32 @@ import (
 type Config struct {
 	Env        string `env:"ENV" env-required:"true"`
 	HTTPServer HTTPServer
+	Postgres   Postgres
 }
 
 type HTTPServer struct {
 	Address     string        `env:"HTTP_SERVER_ADDRESS" env-required:"true"`
 	Timeout     time.Duration `env:"HTTP_SERVER_TIMEOUT"`
 	IdleTimeout time.Duration `env:"HTTP_SERVER_IDLE_TIMEOUT"`
+}
+
+type Postgres struct {
+	// Required
+	Host     string `env:"POSTGRES_HOST" env-required:"true"`
+	Port     int    `env:"POSTGRES_PORT" env-required:"true"`
+	User     string `env:"POSTGRES_USER" env-required:"true"`
+	Password string `env:"POSTGRES_PASSWORD" env-required:"true"`
+	Database string `env:"POSTGRES_DATABASE" env-required:"true"`
+	// Optional
+}
+
+func (p *Postgres) ConnString() string {
+	return fmt.Sprintf(
+		"postgres://%s:%s@%s:%d/%s?sslmode=disable",
+		p.User, p.Password,
+		p.Host, p.Port,
+		p.Database,
+	)
 }
 
 func MustLoad() *Config {
