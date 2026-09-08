6f07b000b5cb1044ae11a00ab8fa8e5508242b86
sudo-odner <odner.main@gmail.com>
2026-09-08 15:09:15 +0300

    feat(service/dm): update migrations files

--- backend/services/dm_service/migrations/000001_dm_tables.down.sql
+++ backend/services/dm_service/migrations/000001_dm_tables.down.sql
@@ -1,3 +1,7 @@
+drop index if exists idx_members_user_id;
+drop index if exists idx_members_channel_id;
+
 drop table if exists members_channel;
 drop table if exists channels;
+
 drop type if exists dm_channel_type;
--- backend/services/dm_service/migrations/000001_dm_tables.up.sql
+++ backend/services/dm_service/migrations/000001_dm_tables.up.sql
@@ -1,20 +1,21 @@
--- Enum type channel
-create type dm_channel_type as enum('dm', 'dm_group');
-
 -- DM Channel
 create table channels (
     id uuid primary key,
     type dm_channel_type not null,
+    name varchar(255),                                      -- Only for dm_group
     updated_at timestamp not null default current_timestamp,
     created_at timestamp not null default current_timestamp
 );
 
 -- DM members
 create table members_channel (
-    id uuid primary key,
     user_id uuid not null,
-    channel_id uuid not null,
-    name varchar(255),                                      -- For DM user can create alias on chat, for DM_GROUP is name chat
+    channel_id uuid not null references channels(id) on delete cascade,
     updated_at timestamp not null default current_timestamp,
-    created_at timestamp not null default current_timestamp
+    created_at timestamp not null default current_timestamp,
+
+    primary key (user_id, channel_id)
 );
+
+create index idx_members_user_id on members_channel(user_id);
+create index idx_members_channel_id on members_channel(channel_id);
