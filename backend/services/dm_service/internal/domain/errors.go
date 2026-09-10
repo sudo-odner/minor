03c4ad7861f4b7b50d7f5f0fb935068c64e257bc
sudo-odner <odner.main@gmail.com>
2026-09-10 21:58:29 +0300

    feat(service/dm): write errors

--- backend/services/dm_service/internal/domain/errors.go
+++ backend/services/dm_service/internal/domain/errors.go
@@ -2,4 +2,7 @@ package domain
 
 import "errors"
 
-var ErrNotFound = errors.New("not found")
+var (
+	ErrNotFound        = errors.New("not found")
+	ErrInvalidArgument = errors.New("invalid argument")
+)
