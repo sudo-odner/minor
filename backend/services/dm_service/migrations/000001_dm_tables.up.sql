e87e0fd5eda1035dc1162df19a5ccd308658e614
sudo-odner <odner.main@gmail.com>
2026-09-12 15:17:30 +0300

    fix(service/dm): fix sql migrations

--- backend/services/dm_service/migrations/000001_dm_tables.up.sql
+++ backend/services/dm_service/migrations/000001_dm_tables.up.sql
@@ -1,11 +1,11 @@
-create type dm_channel_type as enum('DM' 'DM_GROUP')
+create type dm_channel_type as enum('DM', 'DM_GROUP')
 
 -- DM Channel
 create table channels (
     id uuid primary key,
     type dm_channel_type not null,
     name varchar(255),                                      -- Only for dm_group
-    created_at timestamp not null default current_timestamp
+    created_at timestamp not null default current_timestamp,
 
     constraint chk_channel_name_by_type check (
         (type = 'DM' and name is null) or
@@ -22,5 +22,4 @@ create table members_channel (
     primary key (user_id, channel_id)
 );
 
-create index idx_members_user_id on members_channel(user_id);
 create index idx_members_channel_id on members_channel(channel_id);
